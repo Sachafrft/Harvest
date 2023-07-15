e8d218379151cb39617ce37a401b3da171945d2e
Sacha Francfort <sacha.francfort@gmail.com>
2023-07-15 22:37:38 +0200

    on continue

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function calc_Esti_FondCommerce() {
     var radios = document.getElementsByName("zoneActi");
     var selected = Array.from(radios).find(radio => radio.checked);
     if (radios != null) {
-        document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger\" role=\"alert\"> Veuillez selectionner une zone d'activité</div>";
+        document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger m-3\" role=\"alert\"> Veuillez selectionner une zone d'activité</div>";
     }
     zoneActi = selected.value;
     //JE SAIS PAS COMMENT VERIF QUE LE CLIENT CHOISI BIEN UN DES RADIOS psk de base la value de l'element est pas definit dcp le js ne suit pas...
@@ -35,7 +35,7 @@ function calc_Esti_FondCommerce() {
                             
                             document.getElementById("Resultat").innerHTML = '<div class="card border-success mb-3" style="margin-left: auto;margin-right: auto; max-width: 25rem;"><div class="card-header text-bg-success">Estimation de votre fond de commerce</div><div class="card-body"><h5 class="card-title">Note de confiance : 4/5</h5><p class="card-text"> Fourchette de valeur : ' + fourchetteBasse + ' --- ' + fourchetteHaute + '<br><br><i>Si vous souhaiter avoir des conseils et des precisions sur nos calculs n\'hesitez pas à nous contacter :</i></p><a href="#registration" class="btn btn-outline-success">Envoyez nous un message</a></div><img src="../file/Calculatrice3.png" class="card-img-bottom" alt="calculette"></div>';
                         } else {
-                            document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger\" role=\"alert\"> Vous n'avez pas sélectionné de secteur d'activité </div>";
+                            document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger m-3\" role=\"alert\"> Vous n'avez pas sélectionné de secteur d'activité </div>";
                         }
                     }else{
                         messageErreur(loyer);    
@@ -51,13 +51,13 @@ function calc_Esti_FondCommerce() {
             messageErreur(ville);
         }
     } else {
-        document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger\" role=\"alert\"> Veuillez remplir tous les champs </div>";
+        document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger m-3\" role=\"alert\"> Veuillez remplir tous les champs </div>";
     }
 }
 
 
 function messageErreur(nom_input) {
-    document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger\" role=\"alert\">" + nom_input + " est incorrect </div>";
+    document.getElementById("Resultat").innerHTML = "<div class=\"alert alert-danger m-3\" role=\"alert\">" + nom_input + " est incorrect </div>";
 }
 
 function calc_facteur(secteurActi){
